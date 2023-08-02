2795d44a6561fe55c854e471fc892742427f5672
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-02 23:15:42 +0300

    Update Age_calculator.py

--- Age_calculator.py
+++ Age_calculator.py
@@ -1,7 +1,7 @@
 def age_calculator(y, m, d):
     import datetime
-    today = datetime.datetime.now().date()
-    dob = datetime.date(y, m, d)
-    age = int((today-dob).days / 365.25)
+    today = datetime.datetime.now().date()   #get the current date/ todays date
+    now = datetime.date(y, m, d) #object representing the year, month and day
+    age = int((today-now).days / 365.25) #finds the difference
     print(age)
 age_calculator(2001, 12, 3)
