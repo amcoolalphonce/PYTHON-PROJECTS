1c252e9ce535358fd113d89dda022fa609c076ab
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-03 13:45:03 +0300

    Update quizz_option_game.py

--- quizz_option_game.py
+++ quizz_option_game.py
@@ -7,3 +7,9 @@ print('''
 HELLO ! , WELCOME TO MY QUIZ
 EVERY ANSWER IS WORTH 10 POINTS   ''')
 #define a variable for user input
+
+ans=input('ARE YOU READY TO PLAY THE GAME YES/NO    :')
+disc='NOTE: Write the actual answer ,NOT the option'
+score=0                          #initial score is zero before you start.this is very important
+total_questions=5
+# answers=('Python', ' ')      you can predefine all those answers
