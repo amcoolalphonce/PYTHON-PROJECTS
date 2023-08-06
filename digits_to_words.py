314af12a2d2d45a3300222cf837bbcf4ec11b0fc
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-06 21:51:54 +0300

    Create digits_to_words.py

---
+++ digits_to_words.py
@@ -0,0 +1,15 @@
+#transalting digits to words
+phone_no=input(print('ENTER YOUR PHONE NUMBER'))
+
+digits_dictionary={
+    "1":"One",
+    "2":"Two",
+    "3":"Three",
+    "4":"Four",
+    "5":"Five",
+    "6":"Six",
+    "7":"Seven",
+    "8":"Eight",
+    "9":"Nine",
+    "0":"Zero"
+}
