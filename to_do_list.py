2f9424ae190464b5f046dc9893565a4fe3610fa2
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-21 16:27:27 +0300

    Function to display tsks

--- to_do_list.py
+++ to_do_list.py
@@ -2,3 +2,8 @@ tasks = []
 def display_tasks():
     if not tasks:
         print("No tasks in the to-do-list.")
+    else:
+        print("Current tasks:")
+        for index, task in enumerate(tasks, start = 1):
+            print(f'{index}. {tasks}')
+
