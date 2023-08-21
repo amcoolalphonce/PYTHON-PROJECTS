fa33bbe4fc5d711d0530270a754b4fdf8fe02c61
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-21 16:28:35 +0300

    Function to add tasks

--- to_do_list.py
+++ to_do_list.py
@@ -7,3 +7,6 @@ def display_tasks():
         for index, task in enumerate(tasks, start = 1):
             print(f'{index}. {tasks}')
 
+def add_task(task):
+    tasks.append(task)
+    print(f"Task '{task}' added to the to-do list.")
