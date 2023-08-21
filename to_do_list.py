2415bf47e38e040f33fe669ed6f17d03ab05f88c
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-21 16:29:47 +0300

    Function to remove tasks

--- to_do_list.py
+++ to_do_list.py
@@ -10,3 +10,10 @@ def display_tasks():
 def add_task(task):
     tasks.append(task)
     print(f"Task '{task}' added to the to-do list.")
+
+def remove_task(task_index):
+    if 1 <= task_index <= len(tasks):
+        removed_task = tasks.pop(task_index - 1)
+        print(f"Task '{removed_task}' removed from the to-do list.")
+    else:
+        print("Invalid task index.")
