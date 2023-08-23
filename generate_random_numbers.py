19b41c7c9f0addf0f44c22a9f5acab2fe3a8f07e
Alphonce Omondi <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-23 17:09:43 +0300

    Update and rename generate_random_numpers.py to generate_random_numbers.py

--- generate_random_numbers.py
+++ generate_random_numbers.py
@@ -7,6 +7,6 @@ for i in range(3):
     print(random.random())# generates numbers btwn 0 and one
     print(random.randint(3, 20))#btwn 3 and 20
 
-members=['John', 'Okinyi', 'Jackophet', 'Sylvia', 'Kimiti']
+members=['John', 'Price', 'Collins', 'Will', 'Jane']
 leader=random.choice(members)#chooses randomly from the list
 print(leader)
