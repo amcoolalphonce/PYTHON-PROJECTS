9884d242a5260defb74d20fb5e2b317194d771df
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-24 18:57:45 +0300

    Prompt user to enter length of password

--- generate_password.py
+++ generate_password.py
@@ -5,3 +5,5 @@ def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
+
+password_length = int(input("Enter length of the password you want: "))
