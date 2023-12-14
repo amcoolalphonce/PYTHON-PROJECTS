0d978a745bbee5a672a7c8f3f9c5f43e89185044
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-14 20:57:35 +0300

    Update smis.py

--- smis.py
+++ smis.py
@@ -4,3 +4,7 @@ class StudentManagementSystem:
 
         def add_student(self, student_id, name, age, grade):
                 self.students[student_id] = {'Name' : name, 'Age' : age, 'Grade' : grade}
+
+        def view_student(self, student_id):
+                if student_id in self.students:
+                        student_info = self.students[student_id]
