03b88885fd4b63a98602eee8efb4037792f1e50f
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-14 20:59:44 +0300

    Update smis.py

--- smis.py
+++ smis.py
@@ -8,3 +8,5 @@ class StudentManagementSystem:
         def view_student(self, student_id):
                 if student_id in self.students:
                         student_info = self.students[student_id]
+                        if name :
+                                student_info['Nmae'] = name
