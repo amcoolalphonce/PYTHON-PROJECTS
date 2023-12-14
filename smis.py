0e417cdea963b697185cc358a5f4f797383ac0dc
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-14 21:10:20 +0300

    Update smis.py

--- smis.py
+++ smis.py
@@ -30,3 +30,4 @@ class StudentManagementSystem:
         def display_all_students(self):
                 print("All students")
                 for student_id, student_info in self.students.items():
+                        print(f'Student ID: {student_id}, Name : {student_info['Name'] }')
