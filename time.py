8f24078efa2ffe835c0e7165b05d01b147ec807b
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-16 15:06:52 +0300

    Main window
    Signed-off-by: Amcool <82672024+amcoolalphonce@users.noreply.github.com>

--- time.py
+++ time.py
@@ -2,3 +2,7 @@ import tkinter as tk
 import time
 def update_time():
   currrent_time = time.strftime('%H:%M:%S')
+  clock_label.config(text=current_time)
+  root.after(1000, update_time)
+
+root = tk.Tk()
