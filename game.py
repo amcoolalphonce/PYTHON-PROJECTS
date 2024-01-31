144596a241872a16e69e93c34f298a239e8de42c
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-31 19:49:29 +0300

    Update game.py
    Signed-off-by: Amcool <82672024+amcoolalphonce@users.noreply.github.com>

--- game.py
+++ game.py
@@ -3,3 +3,10 @@ print('Welcome to My Quiz')
 answer=input('Are you ready to play the Quiz ? (yes/no) :')
 score=0
 total_questions=3
+if answer.lower()=='yes':
+    answer=input('Question 1: What is your Favourite programming language?')
+    if answer.lower()=='python':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
