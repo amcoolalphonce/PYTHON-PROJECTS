f656fa803887e35c4606cf7905bb33cb85fb7226
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-03-28 05:15:28 +0300

    Other methods
    Signed-off-by: Amcool <82672024+amcoolalphonce@users.noreply.github.com>

--- REVISIT/string_methods.py
+++ REVISIT/string_methods.py
@@ -1,2 +1,7 @@
 my_string = 'Github stores code progress'
+x = 'My name is John Does'
 len(my_string) # returns the length of the string
+x.upper()
+x.lower()
+x.find('s') # false 0
+x.replace('J', 'G')
