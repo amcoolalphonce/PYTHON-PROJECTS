bf6e6d4cd4201b0ada678c851bc9edde20240f5f
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-04-19 13:57:41 +0300

    Forward elimination
    Signed-off-by: Amcool <82672024+amcoolalphonce@users.noreply.github.com>

--- SENIOR YEAR/matrix_solver.py
+++ SENIOR YEAR/matrix_solver.py
@@ -9,3 +9,12 @@ for i in range(row):
         number = float(input(f"Enter the number for matrix[{i + 1}][{j + 1}]: "))
         row_numbers.append(number)
     matrix.append(row_numbers)
+
+# Forward elimination
+for i in range(row - 1): 
+    pivot_row = i
+    while pivot_row < row and matrix[pivot_row][i] == 0:
+        pivot_row += 1
+    if pivot_row == row:
+        print("No unique solution exists")
+        exit()
