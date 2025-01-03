92ece2a81ae1096ec082399bfd5b984870c506de
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2025-01-03 18:23:46 +0300

    Update clock.py
    Signed-off-by: amcool <82672024+amcoolalphonce@users.noreply.github.com>

--- clock.py
+++ clock.py
@@ -1,1 +1,15 @@
+import time
+import tkinter as tk 
 
+def update_time():
+    current_time = time.strftime('%H: %M: %S %p')
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_time)
+
+root = tk.Tk()
+root.title('Digital Clock')
+
+clock_label = tk.Label(root, text ='', font=('Calibri', 40, 'bold'), pady=150, foreground='black')
+clock_label.pack(anchor='center')
+update_time()
+root.mainloop()
